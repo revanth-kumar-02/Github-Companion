0c92281227dd8a5b478e04e68e1300788d526893
Revanth02 <revanthp0201@gmail.com>
2026-06-28 03:39:17 +0530

    variable change

--- commands/pushed.py
+++ commands/pushed.py
@@ -1,14 +1,11 @@
 from services.git_service import get_git_status
 
 
-
-
 def show_gc_push():
     status = get_git_status()
-    push_text = status
     print("Git Push Status")
     print("---------------------------")
-    if "up to date" in push_text:
+    if "up to date" in status:
         print("✅ Latest commit is pushed.")
     else:
         print("⚠ Latest commit is NOT pushed.")
